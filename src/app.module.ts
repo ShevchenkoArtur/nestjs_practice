e496a8ea9b47179664b20ee608a4a948b5316c0e
Artur <shevchenkoartur745@gmail.com>
2021-12-19 14:16:39 +0200

    worked with env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,23 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {SequelizeModule} from "@nestjs/sequelize";
+import {ConfigModule} from "@nestjs/config";
 
 @Module({
-  imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'root',
-      database: 'nestjs-practice',
-      models: [],
-      autoLoadModels: true,
-    }),
-  ],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
+            models: [],
+            autoLoadModels: true,
+        }),
+    ],
 })
-export class AppModule {}
+export class AppModule {
+}
