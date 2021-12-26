e5615c24598f1fcdca4041685ec24108a26f3f15
Artur <shevchenkoartur745@gmail.com>
2021-12-26 13:16:26 +0200

    binary search

--- linear_search.js
+++ linear_search.js
@@ -1,21 +1,15 @@
-const array = [2, 5, 6, 7, 3, 1, 4, 8, 10, 9];
+const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let count = 0;
 
 function linearSearch(array, item) {
-    let count = 0;
-
     for (let i = 0; i < array.length; i++) {
         count++;
         if (array[i] === item) {
-            return {
-                itemIndex: i,
-                countOperation: count,
-            };
+            return i;
         }
     }
-    return {
-        itemIndex: null,
-        countOperation: count,
-    };
+    return null;
 }
 
-console.log(linearSearch(array, 9));
+console.log(linearSearch(array, 15));
+console.log(count);
