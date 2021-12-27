202928497c4ea899a52382abd6a54ddf305c97eb
Artur Shevchenko <shevchenko-a@bubuntu-lx.nixsolutions.com>
2021-12-27 10:51:15 +0200

    added gateway loggers

--- server/src/app.gateway.ts
+++ server/src/app.gateway.ts
@@ -1,9 +1,41 @@
-import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Logger } from '@nestjs/common';
+import { Socket, Server } from 'socket.io';
 
 @WebSocketGateway()
-export class AppGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: Server): any {
+    this.logger.log('Initialized!');
   }
+
+  handleConnection(client: Socket, ...args: any[]): any {
+    this.logger.log(`Client connected ${client.id}`);
+  }
+
+  handleDisconnect(client: Socket): any {
+    this.logger.log(`Client disconnected ${client.id}`);
+  }
+
+  // V1 WITH TYPES
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, text: string): WsResponse<string> {
+    return { event: 'msgToClient', data: 'Hello world!' };
+  }
+
+  // V2 WITHOUT TYPES
+  // @SubscribeMessage('msgToServer')
+  // handleMessage(client: Socket, text: string): void {
+  //   client.emit('msgToClient', text);
+  // }
 }
